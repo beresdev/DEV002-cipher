e0640b3eb43c1d7d65a7577b52e7d559c6ae14d5
beresdev <beresdev@gmail.com>
2022-10-21 13:29:59 -0500

    Se comentan las lineas en el archivo cipher.js. Se agregan id a los botones de la pagina principal, index.html. En index.js se comentan las lineas de import y console, se agrega la lógica para que los dos botones nos lleven a una nueva ventana code.html y decode.html respectivamente

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import cipher from './cipher.js';
+// import cipher from './cipher.js';
 
-console.log(cipher);
+// console.log(cipher);
+
+const botonCifrar = document.getElementById("cbutton");
+const botonDescifrar = document.getElementById("dbutton");
+
+botonCifrar.addEventListener("click", function() {
+    window.open("./code.html")
+})
+
+botonDescifrar.addEventListener("click", function() {
+    window.open("./decode.html")
+})
