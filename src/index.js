7f4f0b50e8fb0a5c7d43aa9094d8197da39bc651
beresdev <beresdev@gmail.com>
2022-10-25 19:11:41 -0500

    Se agrega la lógica para el cifrado y descifrado a index.js. Se cambian nombres de variables para no duplicar. En index.js tambien se agrega una validación para la función de cifrado de modo que no acepte valores de clave con modulo 26 === 0 porque el cifrado queda exactamente igual. En index.html se cambian id's para no tener valores duplicados ya que ahora todo se tiene en un mismo archivo html. Se comenta todo el código de los archivos js encode y decode. En este commit ya queda funcionando la web con cifrado y descifrado. Aun queda pendiente darle funcionalidad a los nuevos botones CIFRAR y DESCIFRAR(end-buttons) de las secciones encode y decode

--- src/index.js
+++ src/index.js
@@ -5,20 +5,102 @@
 document.getElementById("encode-section").style.display = "none";
 document.getElementById("decode-section").style.display = "none";
 
-const botonCifrar = document.getElementById("cbutton");
-const botonDescifrar = document.getElementById("dbutton");
 
-botonCifrar.addEventListener("click", function() {
+
+const botonCifrarI = document.getElementById("cbutton");
+const botonDescifrarI = document.getElementById("dbutton");
+const botonCifrarS = document.getElementById("cbuttonS");
+const botonDescifrarS = document.getElementById("dbuttonS");
+
+let inputC = document.getElementById("keyC");
+let inputD = document.getElementById("keyD");
+
+let text = document.getElementById("originalText");
+let ctext = document.getElementById("cipherText");
+
+let ctextD = document.getElementById("cipherTextD");
+let dctext = document.getElementById("decipherText");
+
+
+botonCifrarI.addEventListener("click", function() {
     document.getElementById("main-section").style.display = "none";
     document.getElementById("encode-section").style.display = "flex";
     document.getElementById("decode-section").style.display = "none";
 })
 
-botonDescifrar.addEventListener("click", function() {
+botonCifrarS.addEventListener("click", cifrarMensaje)
+
+function cifrarMensaje() {
+    if (inputC.value === ''|| inputC.value === 0 || inputC.value === NaN) {
+        alert("Especifica clave de cifrado")
+    } else if (text.value === '') {
+        alert("Agrega texto a cifrar")
+    }
+
+    let valor = parseInt(inputC.value);
+    let otext = text.value;
+    let textInASCII = [];
+    let codeTextInASCII = [];
+    let codeText = []; 
+
+    if (valor % 26 === 0) {
+        alert("Por seguridad ingresa un valor distinto")
+    } else {
+        for (let i=0; i < otext.length; i++) {
+            let x = otext.charCodeAt(i);
+            textInASCII.push(x)
+            if (textInASCII[i] === 32) {
+                let l = 32;
+                codeTextInASCII.push(l);
+            } else {
+                let l = ((textInASCII[i] - 65 + valor) % 26) + 65;
+                codeTextInASCII.push(l);
+            }
+            let y = String.fromCharCode(codeTextInASCII[i]);
+            codeText.push(y);
+        }
+        ctext.innerHTML = codeText.join('');
+    }
+}
+
+botonDescifrarI.addEventListener("click", function() {
     document.getElementById("main-section").style.display = "none";
     document.getElementById("decode-section").style.display = "flex";
     document.getElementById("encode-section").style.display = "none";
     // window.open("./decode.html")
 })
+
+botonDescifrarS.addEventListener("click", descifrarMensaje);
+
+function descifrarMensaje() {
+    if (inputD.value === ''|| inputD.value === 0 || inputD.value === NaN) {
+        alert("Especifica clave de descifrado")
+    } else if (ctextD.value === '') {
+        alert("Agrega texto a descifrar")
+    }
+
+    let n = parseInt(inputD.value);
+    let principalText = ctextD.value;
+    let textInASCII = [];
+    let decodeTextInASCII = [];
+    let decodeText = [];
+
+    for (let i = 0; i < principalText.length; i++) {
+        let x = principalText.charCodeAt(i);
+        textInASCII.push(x);
+
+        if(textInASCII[i] === 32) {
+            let l = 32;
+            decodeTextInASCII.push(l);
+        } else {
+            let l = ((textInASCII[i] + 65 - n) % 26) + 65;
+            decodeTextInASCII.push(l);
+        }
+
+        let y = String.fromCharCode(decodeTextInASCII[i]);
+        decodeText.push(y);
+    }
+    dctext.innerHTML = decodeText.join('');
+}
 
 
